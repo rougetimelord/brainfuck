33ea28961780df292057bcb577e0aec52215b4d7
Rouge <code@r0uge.org>
2017-10-24 11:47:00 -0700

    Add code

---
+++ brainfuck.py
@@ -0,0 +1,94 @@
+"""BrainFucK"""
+import re
+import sys
+
+JMP_TABLE = {}
+def setup():
+    """Sanitize code"""
+    code = list(re.sub(r'[^<>+-.,\[\]]', '', sys.argv[1] if len(sys.argv) > 1
+                       else input("Code:\n")))
+    op = []
+    for i, c in enumerate(code):
+        if c == '[':
+            op.append(i)
+        if c == ']':
+            try:
+                out = op.pop()
+                JMP_TABLE[i] = out
+                JMP_TABLE[out] = i
+            except IndexError:
+                op.append(i)
+    if op:
+        exit()
+    main(code)
+
+code_ptr = 0
+data_ptr = 0
+def main(code):
+    """Run code"""
+    global code_ptr
+    tape = {}
+    def ptr_i():
+        """Tape head++"""
+        global data_ptr
+        data_ptr += 1
+        return
+    def ptr_d():
+        """Tape head--"""
+        global data_ptr
+        data_ptr -= 1
+        return
+    def add():
+        """Cell++"""
+        if data_ptr in tape:
+            tape[data_ptr] += 1
+        else:
+            tape[data_ptr] = 1
+        return
+    def sub():
+        """Cell--"""
+        if data_ptr in tape:
+            tape[data_ptr] -= 1
+        else:
+            tape[data_ptr] = 0
+        return
+    def outp():
+        """Output"""
+        try:
+            print(chr(tape[data_ptr]), end='', flush=True)
+        except OSError as e:
+            print("OS Exception %s" % e)
+        return
+    def inp():
+        """Input"""
+        x = input()
+        try:
+            x = int(x)
+        except ValueError:
+            x = ord(x)
+        tape[data_ptr] = x
+        return
+    def jmp_i():
+        """Jump if 0"""
+        global code_ptr
+        code_ptr = ((JMP_TABLE[code_ptr] - 1) if data_ptr not in tape or not tape[data_ptr]
+                    else code_ptr)
+        return
+    def jmp_o():
+        """Jump if 1"""
+        global code_ptr
+        code_ptr = (JMP_TABLE[code_ptr] - 1) if data_ptr in tape and tape[data_ptr] else code_ptr
+        return
+    inst_tbl = {'>': ptr_i,
+                '<': ptr_d, '+': add, '-': sub,
+                '.': outp, ',': inp, '[': jmp_i,
+                ']': jmp_o}
+    while code_ptr < len(code):
+        inst_tbl[code[code_ptr]]()
+        code_ptr += 1
+    input("Done, hit enter to exit")
+
+if __name__ == '__main__':
+    setup()
+else:
+    main(list(sys.argv[1]))
