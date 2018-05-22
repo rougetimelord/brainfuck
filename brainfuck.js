2251e3f99721472bc36c5da8e08148cf3776df3e
Rouge <code@r0uge.org>
2018-05-21 19:35:45 -0700

    Export I/O functions

--- brainfuck.js
+++ brainfuck.js
@@ -2,7 +2,7 @@
 Basically set up a tape, tape head, code pointer, and an instruction table full of arrow functions
 Then go through all of the code and execute the arrow function*/
 var interpretter=(c,j)=>{
-    for(var cp=0,t=[],dp=0,ins={'<':()=>dp--,'>':()=>dp++,'+':()=>{t[dp]?t[dp]++:t[dp]=1},'-':()=>{t[dp]?t[dp]--:t[dp]=0},'.':()=>window.alert(String.fromCharCode(t[dp])),',':()=>t[dp]=parseInt(window.prompt(''),10),'[':()=>cp=t[dp]?cp:jt[cp],']':()=>cp=t[dp]?jt[cp]:cp};cp<c.length;cp++)ins[c[cp]]();
+    for(var cp=0,t=[],dp=0,ins={'<':()=>dp--,'>':()=>dp++,'+':()=>{t[dp]?t[dp]++:t[dp]=1},'-':()=>{t[dp]?t[dp]--:t[dp]=0},'.':()=>io.o(t[dp]),',':()=>t[dp]=io.i(),'[':()=>cp=t[dp]?cp:jt[cp],']':()=>cp=t[dp]?jt[cp]:cp};cp<c.length;cp++)ins[c[cp]]();
 }
 /*Split string and filter everything that isn't BF out
 Then setup a jump table and start interpretting*/
@@ -13,3 +13,12 @@ var setup=(t)=>{
     jt={};if(a&&b&&a.length===b.length){(a.forEach((v,i)=>{jt[v]=b[i];jt[b[i]]=v}))};
     interpretter(c,jt);
 }
+/*Export I/O so that it can be overwritten*/
+var io = {
+    o:t=>{
+        window.alert(String.fromCharCode(t));
+    },
+    i:()=>{
+        return parseInt(window.prompt(''), 10);
+    }
+};
