7bb1eff9ce1efd22411a6fccdf8a218f44455821
Rouge <blairwarkii@gmail.com>
2018-09-11 20:00:45 -0700

    Make class

--- brainfuck.js
+++ brainfuck.js
@@ -1,24 +1,20 @@
-/*What's less readable brainfuck or this interpretter?
-Basically set up a tape, tape head, code pointer, and an instruction table full of arrow functions
-Then go through all of the code and execute the arrow function*/
-var interpretter=(c,j)=>{
-    for(var cp=0,t=[],dp=0,ins={'<':()=>dp--,'>':()=>dp++,'+':()=>{t[dp]?t[dp]++:t[dp]=1},'-':()=>{t[dp]?t[dp]--:t[dp]=0},'.':()=>io.o(t[dp]),',':()=>t[dp]=io.i(),'[':()=>cp=t[dp]?cp:jt[cp],']':()=>cp=t[dp]?jt[cp]:cp};cp<c.length;cp++)ins[c[cp]]();
-}
-/*Split string and filter everything that isn't BF out
-Then setup a jump table and start interpretting*/
-var setup=(t)=>{
-    c=t.split('').map(s=>s.trim()).filter(s=>/[<>+-.,\[\]]/.test(s));
-    a=[];r=/\[/g;while((m=r.exec(t))!=null)a.push(m.index);
-    b=[];r=/\]/g;while((m=r.exec(t))!=null)b.push(m.index);
-    jt={};if(a&&b&&a.length===b.length){(a.forEach((v,i)=>{jt[v]=b[i];jt[b[i]]=v}))};
-    interpretter(c,jt);
-}
-/*Export I/O so that it can be overwritten*/
-var io = {
-    o:t=>{
-        window.alert(String.fromCharCode(t));
-    },
-    i:()=>{
-        return parseInt(window.prompt(''), 10);
+//What's less readable brainfuck or this interpretter?
+var brainfuck = class {
+    /*Split string and filter everything that isn't BF out
+    Then setup a jump table and I/O*/
+    constructor(t,i=false,o=false){
+        this.c=t.split('').map(s=>s.trim()).filter(s=>/[<>+-.,\[\]]/.test(s));
+        let a=[];let r=/\[/g, m = '';while((m = r.exec(t))!=null)a.push(m.index);
+        let b=[];r=/\]/g;while((m = r.exec(t))!=null)b.push(m.index);
+        this.jt={};
+        if(a&&b&&a.length===b.length){
+            a.forEach((v,i)=>{this.jt[v]=b[i];this.jt[b[i]]=v})};
+        this.o = (!o) ? t=>{window.alert(String.fromCharCode(t))} : o;
+        this.i = (!i) ? ()=>{return parseInt(window.prompt(''), 10)} : i;
     }
-};
+    /*Basically set up a tape, tape head, code pointer, and an instruction table full of arrow functions
+    Then go through all of the code and execute the arrow function*/
+    run(d=false,df=console.log){
+        for(let cp=0,t=[],dp=0,ins={'<':()=>dp--,'>':()=>dp++,'+':()=>{t[dp]?t[dp]++:t[dp]=1},'-':()=>{t[dp]?t[dp]--:t[dp]=0},'.':()=>this.o(t[dp]),',':()=>t[dp]=this.i(),'[':()=>cp=t[dp]?cp:this.jt[cp],']':()=>cp=t[dp]?this.jt[cp]:cp};cp<this.c.length;cp++){ins[this.c[cp]]();if(d){df(t,cp,dp)};}
+    }
+}
